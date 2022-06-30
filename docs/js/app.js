190028b214cdf4b44157fa4c74847380eaab0611
EkaterinaKochneva <kochnevaes@gmail.com>
2022-06-30 17:50:28 +0300

    header and footer, start main

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,3 +1,27 @@
-import * as flsFunctions from "./modules/functions.js";
+window.onload = function () {
 
-flsFunctions.isWebp();
+	// Кнопки в шапке
+
+	$(".body-header__search-mobile").click(function (e) {
+		$(this).toggleClass("active");
+		$(".body-header__search").slideToggle().find('input').focus();
+	});
+
+	$('.body-header__menu-mobile').click(function () {
+		$('.bottom-header').toggleClass('active');
+		$('body').toggleClass('fixed-mb');
+	});
+	$('.bottom-header__menu-close-mob').click(function () {
+		$('.bottom-header').removeClass('active');
+		$('body').removeClass('fixed-mb');
+	});
+
+    $('.header-nav__link-icon').click(function () {		
+		let parent = $(this).parents('.header-nav__item--secondary');
+        parent.toggleClass("active");
+		parent.children(".header-nav__submenu--secondary").slideToggle();
+	});
+
+	
+
+}
